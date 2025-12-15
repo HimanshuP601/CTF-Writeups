2597e330ec2b338d33577c7740106b0c64237ac3
HimanshuP601 <himanshuparate601@gmail.com>
2025-12-15 20:17:11 +0530

    Reuploaded writeup

--- cryptopals/Set-1/codes/hex2base64.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-hex_str = input("Enter hex value:")
-byte_str = bytes.fromhex(hex_str)
-b64_str = base64.b64encode(byte_str)
-print(f"\nHex:{hex_str}")
-print(f"Base64:{b64_str.decode()}")
